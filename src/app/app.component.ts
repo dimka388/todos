94d9806ac25da0d664aa9a84251555647e9fc3c2
Dmytro Rozhkov <dmytro.rozhkov@synapse.com>
2017-09-13 18:41:14 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ import 'rxjs/add/operator/map';
 
 export class AppComponent {
 	src = 'https://59b68e8ed7d6800011e572f5.mockapi.io/items';
+	priorityList = ['low', 'medium', 'high'];
 	items = [];
 
 	getItems(): Observable <any> {
@@ -29,13 +30,68 @@ export class AppComponent {
 			.subscribe(() => console.warn('Item removed:' + obj.value));
 	}
 
-	onChangeStatus(e, obj) {
+	itemDone(obj) {
 		this.items.map((item) => {
 			if (item.id === obj.id) {
-				item.complete = e.target.checked;
+				item.complete = !item.complete;
 				this.http.put(this.src + '/' + item.id, item)
 					.subscribe(() => console.warn('Item updated:' + item.value));
 			}
+		});
+	}
+
+	itemPriorityChange(e, obj) {
+		this.items.map((item) => {
+			if (item.id === obj.id) {
+				item.priority = e.target.value;
+				this.http.put(this.src + '/' + item.id, item)
+					.subscribe(() => console.warn('Item updated:' + item.value));
+			}
+		});
+	}
+
+	itemOrderChange(currentItem, otherItems, decrement) {
+		let newIndex = null;
+		let updatedItems = [];
+		let getIndex = (index) => {
+			if (decrement) {
+				return newIndex === null || index >= newIndex;
+			} else {
+				return newIndex === null || newIndex > index;
+			}
+		}
+		if (otherItems.length) {
+			if (decrement) {
+				otherItems.sort((a: any, b: any) => b.index - a.index);
+			}
+			otherItems.map(item => {
+				if (getIndex(item.index)) {
+					newIndex = item.index;
+					item.index = currentItem.index;
+					updatedItems.push(item);
+				}
+			});
+			currentItem.index = newIndex;
+			updatedItems.push(currentItem);
+			this.onChangeOrder(updatedItems);
+		}
+	}
+
+	itemUp(currentItem) {
+		this.itemOrderChange(currentItem, this.items.filter(item => item.index < currentItem.index), true);
+	}
+	
+	itemDown(currentItem) {
+		this.itemOrderChange(currentItem, this.items.filter(item => item.index > currentItem.index), false);
+	}
+
+	onChangeOrder(items: any) {
+		items.map(item => {
+			this.http.put(this.src + '/' + item.id, item)
+			.subscribe(() => console.warn('Item updated:' + item.value));
+		});
+		this.items.sort((a: any, b: any) => {
+			return a.index - b.index;
 		});
 	}
 
@@ -44,9 +100,12 @@ export class AppComponent {
 		const newItem = {
 			id: this.items.length + 1,
 			value: itemForm.value.text,
-			priority: itemForm.value.priority || 'normal',
+			priority: itemForm.value.priority || this.priorityList[0],
 			index: this.items.length + 1
 		};
+		if (!itemForm.value.text.length) {
+			return;
+		}
 		this.items.push(newItem);
 		this.http.post(this.src, newItem)
 			.subscribe(() => console.warn('Item added:' + newItem.value));
@@ -55,8 +114,8 @@ export class AppComponent {
 
 	constructor(private http:Http) {
 		this.getItems().subscribe(data => {
-			data.sort((a, b) => {
-				return +a.index > +b.index;
+			data.sort((a: any, b: any) => {
+				return a.index - b.index;
 			});
 			this.items = data;
 		});
